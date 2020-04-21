17cdaadb1de70f4e43ebaa696e6033e650172891
kjunha <kjunha77@gmail.com>
2020-04-22 01:33:45 +0900

    add app data update logic & refactor result textfield

---
+++ app/src/main/java/com/example/currencyexchange/ConnectionManager.kt
@@ -0,0 +1,13 @@
+package com.example.currencyexchange
+
+class ConnectionManager {
+    private var lastUpdate:String? = null
+    //Singleton Object (Composition Model)
+    companion object {
+        val instance = ConnectionManager()
+    }
+
+    public fun updateCurrency(requested: String) {
+
+    }
+}
--- app/src/main/java/com/example/currencyexchange/MainActivity.kt
+++ app/src/main/java/com/example/currencyexchange/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.currencyexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import android.widget.AdapterView
@@ -13,30 +15,32 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), TextWatcher {
     private lateinit var senderSpinner:Spinner
     private lateinit var receiverSpinner:Spinner
     private lateinit var exchangeRateTextView:TextView
     private lateinit var requestTimeTextView:TextView
     private lateinit var amountEditText:EditText
+    private lateinit var resultTextView:TextView
 
-    private var countryList = arrayOf<String>("한국(KRW)", "일본(JPN)", "필리핀(PHP)")
     private var dateTimeFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
+        update()
     }
     private fun initViews() {
+        //Late initialization
         senderSpinner = findViewById(R.id.sender)
         receiverSpinner = findViewById(R.id.receiver)
         exchangeRateTextView = findViewById(R.id.exchange_rate)
         requestTimeTextView = findViewById(R.id.request_time)
         amountEditText = findViewById(R.id.amount)
+        resultTextView = findViewById(R.id.result_text)
 
-        requestTimeTextView.setText(LocalDateTime.now().format(dateTimeFormat))
-
+        //Apply Listener
         receiverSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
             override fun onItemSelected(
                 parent: AdapterView<*>?,
@@ -47,11 +51,14 @@ class MainActivity : AppCompatActivity() {
                 exchangeRateTextView.setText(getCurrencyCode(receiverSpinner.selectedItem.toString())
                         + "/"
                         + getCurrencyCode(senderSpinner.selectedItem.toString()))
+                update()
             }
             override fun onNothingSelected(parent: AdapterView<*>?) {}
         }
+        amountEditText.addTextChangedListener(this)
     }
 
+    //Utility Functions
     private fun getCurrencyCode(country: String):String {
         val builder = StringBuilder()
         for((index, char) in country.withIndex()) {
@@ -64,4 +71,16 @@ class MainActivity : AppCompatActivity() {
         }
         return builder.toString()
     }
+
+    private fun update() {
+        requestTimeTextView.setText(LocalDateTime.now().format(dateTimeFormat))
+        resultTextView.setText(resources.getString(R.string.result_message_1) + " 10000 " + resources.getString(R.string.result_message_2))
+    }
+
+    override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        update()
+    }
+
+    override fun afterTextChanged(s: Editable?) {}
+    override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 }
