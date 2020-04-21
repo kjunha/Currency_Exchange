89bdf4db3e716720fe85315e22aac7c2a09b5f17
kjunha <kjunha77@gmail.com>
2020-04-22 00:46:01 +0900

    replace sender, receiver textfields to spinner & add spinner listener

--- app/src/main/java/com/example/currencyexchange/MainActivity.kt
+++ app/src/main/java/com/example/currencyexchange/MainActivity.kt
@@ -2,13 +2,20 @@ package com.example.currencyexchange
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import android.widget.EditText
+import android.widget.Spinner
 import android.widget.TextView
+import java.lang.StringBuilder
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var receiverTextView:TextView
+    private lateinit var senderSpinner:Spinner
+    private lateinit var receiverSpinner:Spinner
     private lateinit var exchangeRateTextView:TextView
     private lateinit var requestTimeTextView:TextView
     private lateinit var amountEditText:EditText
@@ -22,12 +29,39 @@ class MainActivity : AppCompatActivity() {
         initViews()
     }
     private fun initViews() {
-        receiverTextView = findViewById(R.id.receiver)
+        senderSpinner = findViewById(R.id.sender)
+        receiverSpinner = findViewById(R.id.receiver)
         exchangeRateTextView = findViewById(R.id.exchange_rate)
         requestTimeTextView = findViewById(R.id.request_time)
         amountEditText = findViewById(R.id.amount)
 
-        receiverTextView.setText(countryList[0])
         requestTimeTextView.setText(LocalDateTime.now().format(dateTimeFormat))
+
+        receiverSpinner.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                exchangeRateTextView.setText(getCurrencyCode(receiverSpinner.selectedItem.toString())
+                        + "/"
+                        + getCurrencyCode(senderSpinner.selectedItem.toString()))
+            }
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+    }
+
+    private fun getCurrencyCode(country: String):String {
+        val builder = StringBuilder()
+        for((index, char) in country.withIndex()) {
+            if(char == '(') {
+                builder.append(country.get(index+1))
+                builder.append(country.get(index+2))
+                builder.append(country.get(index+3))
+                break
+            }
+        }
+        return builder.toString()
     }
 }
